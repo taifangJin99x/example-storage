a3bff122eb5324243e85814e9ad6242712407bd8
e <wwcxmry@163.com>
2021-01-19 14:38:46 +0800

    服务端nacos

--- src/main/java/com/example/storage/StorageApplication.java
+++ src/main/java/com/example/storage/StorageApplication.java
@@ -1,10 +1,16 @@
 package com.example.storage;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import javax.annotation.PostConstruct;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -15,5 +21,16 @@ public class StorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class, args);
     }
+    @NacosInjected
+    private NamingService namingService;
 
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        namingService.registerInstance(applicationName,"47.103.133.107",serverPort);
+    }
 }
