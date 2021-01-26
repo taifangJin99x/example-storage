21ce0fc5c6a6dbfb4945db4c60e48e332687663a
e <wwcxmry@163.com>
2021-01-26 11:10:28 +0800

    seata

--- src/main/java/com/example/storage/api/StorageController.java
+++ src/main/java/com/example/storage/api/StorageController.java
@@ -1,9 +1,12 @@
 package com.example.storage.api;
 
 import com.example.storage.service.StorageService;
+import feign.Param;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +32,17 @@ public class StorageController {
     public String test() {
         log.info("TEST");
         return storageService.selectOne();
+    }
+
+    @GetMapping("/deStorage")
+    public String deStorage(@RequestParam("code") String code) {
+        log.info("deStorage:{}",code);
+        return storageService.deStorage(code);
+    }
+
+    @GetMapping("/deStorageError")
+    public String deStorageError(@RequestParam("code") String code) {
+        log.info("deStorageError:{}",code);
+        return storageService.deStorageError(code);
     }
 }
--- src/main/java/com/example/storage/service/StorageService.java
+++ src/main/java/com/example/storage/service/StorageService.java
@@ -1,5 +1,6 @@
 package com.example.storage.service;
 
+import com.example.storage.entity.Storage;
 import com.example.storage.repository.StorageDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,18 @@ public class StorageService {
     public String selectOne() {
        return storageDao.selectById("1").toString();
 
+    }
+
+    public String deStorage(String code) {
+        Storage storage = new Storage();
+        storage.setCommodityCode(code);
+        storage.setCount(1L);
+        storageDao.insert(storage);
+        return "ok";
+    }
+
+    public String deStorageError(String code) {
+        int i = 0/1;
+        return "";
     }
 }
